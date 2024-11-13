10a8968841a80df5f4cd7e957d47199e8291df6d
Vamsi Penumatsa <vamsi.penumatsa@motivitylabs.com>
2024-11-13 16:02:46 +0530

    code updated

--- src/test/java/com/ivis/iportal/script/TC_IportalAutomation.java
+++ src/test/java/com/ivis/iportal/script/TC_IportalAutomation.java
@@ -15,29 +15,34 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class TC_IportalAutomation extends TestBase {
-    public static String file_location = System.getProperty("user.dir") + "\\Resources\\IvisUnitConfiguration.xlsx";
+    public static String customer_file_location = System.getProperty("user.dir") + "\\Resources\\Customer.xlsx";
+    public static String project_file_location = System.getProperty("user.dir") + "\\Resources\\Project.xlsx";
+    public static String tenant_file_location = System.getProperty("user.dir") + "\\Resources\\Tenant.xlsx";
+    public static String unit_file_location = System.getProperty("user.dir") + "\\Resources\\Unit.xlsx";
+    public static String userMgmt_file_location = System.getProperty("user.dir") + "\\Resources\\UserMgmt.xlsx";
 
     public Object[][] arrObj;
 
     public HashMap<String, String> map = new HashMap<String, String>();
 
     @DataProvider(parallel = false)
-    public Object[][] loginValidation() throws IOException {
+    public Object[][] customerValidation() throws IOException {
         // We are creating an object from the excel sheet data by calling a method that
         // reads data from the excel stored locally in our system
-        arrObj = getExcelData(file_location, "Userlogin");
+        arrObj = getExcelData(customer_file_location, "Customer");
         return arrObj;
     }
-  /*  @DataProvider(parallel = false)
-    public Object[][] sitesCreationPage() throws IOException {
+
+    @DataProvider(parallel = false)
+    public Object[][] projectValidation() throws IOException {
         // We are creating an object from the excel sheet data by calling a method that
         // reads data from the excel stored locally in our system
-        arrObj = getExcelData(file_location, "Sites");
+        arrObj = getExcelData(project_file_location, "Project");
         return arrObj;
-    }*/
+    }
 
-    @Test(priority = 0, dataProvider = "loginValidation")
-    public void iportalLoginValidation(String url, String runmode, String user, String pass, String invalidpass, String customerName,String project, String siteGroup) throws Throwable {
+    @Test(priority = 0, dataProvider = "customerValidation")
+    public void iportalLoginValidation(String url, String runmode, String user, String pass, String customerName,String project, String siteGroup) throws Throwable {
         try {
             if (runmode.equalsIgnoreCase("Yes")) {
 
@@ -45,7 +50,7 @@ public class TC_IportalAutomation extends TestBase {
                 loginLib lb = new loginLib(WEBDRIVER_THREADLOCAL.get());
                 createSites cs = new createSites(WEBDRIVER_THREADLOCAL.get());
                 lb.invalidLogin(url, user, invalidpass);
-                lb.validLogin(url, user, pass);
+                lb.validLogin(user, pass);
                 cs.clickSites();
                 cs.addSites(customerName, project, siteGroup);
                 System.out.println("test methods executed successfully");
@@ -56,23 +61,6 @@ public class TC_IportalAutomation extends TestBase {
         }
 
     }
-  /*  @Test(priority = 1,dataProvider = "loginValidation")
-    public void sitesCreationPage(String url, String runmode, String user, String pass, String customerName,String project, String siteGroup) throws Throwable{
-        try {
-            if (runmode.equalsIgnoreCase("Yes")) {
-                //createSites cs = new createSites(WEBDRIVER_THREADLOCAL.get());
-                System.out.println("Dead");
-                *//*iportalLoginValidation(url, runmode, user, pass);
-                cs.clickSites();*//*
-                //s.addSites(customerName, project, siteGroup);
-                System.out.println("test methods executed successfully");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }*/
 
 
     public synchronized String[][] getExcelData(String fileName, String sheetName) throws IOException {
